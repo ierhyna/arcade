0f43775d66f0ddcf3ba33808f702afb4cccdbfa4
Irina Sokolovskaja <sokolovskaja.irina@gmail.com>
2017-06-10 22:37:47 +0300

    fixed moving

--- js/stages/Level.stage.js
+++ js/stages/Level.stage.js
@@ -25,7 +25,7 @@ export const Level = {
       // walls.debug = true;
       bg.resizeWorld();
 
-      game.physics.arcade.gravity.y = 500;
+      game.physics.arcade.gravity.y = 1000;
 
       player = game.add.sprite(32, 32, 'hero');
       game.physics.enable(player, Phaser.Physics.ARCADE);
@@ -42,13 +42,13 @@ export const Level = {
       player.body.velocity.x = 0;
 
       if (cursors.left.isDown) {
-        player.body.velocity.x = -150;
+        player.body.velocity.x = -220;
       } else if (cursors.right.isDown) {
-        player.body.velocity.x = 150;
+        player.body.velocity.x = 220;
       }
 
       if (cursors.up.isDown && player.body.onFloor() && game.time.now > jumpTimer) {
-        player.body.velocity.y = -410;
+        player.body.velocity.y = -580;
         jumpTimer = game.time.now + 750;
       }
     },
