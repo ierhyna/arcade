89101df78f32d0a5c06b860b2ac27d59b569b336
Alexei Bykov <spbeat@mail.ru>
2017-07-05 23:57:21 +0300

    better pick up

--- js/classes/Coin.class.js
+++ js/classes/Coin.class.js
@@ -10,18 +10,20 @@ export default class Coin extends GameObject {
         this.item = new Item(data);
         this.game.physics.enable(this);
         this.body.allowGravity = true;
+        this.active = true;
     };
 
     spawn(x, y) {
         this.classReset(x, y);
+        this.active = true;
     };
 
     update() {
         this.game.physics.arcade.collide(this, game.walls)
         this.game.physics.arcade.overlap(this, game.player, () => {
-            console.log("ate coin worth of " + this.value);
+            if (!this.active) return;
             Text.combat(this, `+${this.value} gold`, "info");
-            this.kill();
+            this.die();
         });
     };
 
@@ -31,6 +33,14 @@ export default class Coin extends GameObject {
         this.value = 0;
         this.scale.setTo(0.25, 0.25);
         this.exists = true;
+        this.active = true;
         this.game.add.existing(this);
+    }
+
+    die() {
+        this.active = false;
+        const direction = { y: this.y - 150, alpha: 0 }
+        const tween = game.add.tween(this).to(direction, 1000, "Linear", true);
+        tween.onComplete.addOnce(() => this.destroy());
     }
 }
