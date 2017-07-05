73348c2bd8c20e9ffff794b677dbb3515dc0f936
Alexei Bykov <spbeat@mail.ru>
2017-07-05 23:46:11 +0300

    polishing

--- js/classes/Blob.class.js
+++ js/classes/Blob.class.js
@@ -8,15 +8,15 @@ export default class Blob extends Enemy {
         super(sprite);
         this.game = game;
         this.item = new Item(data);
-        this.sound = this.game.add.audio('mobHit');
+        this.hitSound = this.game.add.audio('mobHit');
         this.animations.add("live", [0, 1], 10, true);
         this.animations.add("die", [2, 3, 4, 5, 6], 10, true);
         this.animations.add("blink", [7, 0], 10);
         this.scale.setTo(1, 2);
-        this.maxHealth = 125;
-        this.damageOnContact = 125;
+        this.maxHealth = 100;
+        this.damageOnContact = 75;
         this.exp = 75;
-        this.speed = 120;
+        this.speed = 180;
     };
 
     spawn(x, y) {
