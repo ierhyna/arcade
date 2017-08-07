c8907ea34e8861af067fabdb9dff90814daf38db
tomkallen <tom9433021>
2017-08-07 21:38:47 +0300

    some fixes

--- js/classes/Projectile.class.js
+++ js/classes/Projectile.class.js
@@ -21,12 +21,18 @@ export default class Projectile extends Phaser.Sprite {
     };
 
     calculateDamage(){
-        const multplier = this.critical ? this.criticalMultiplier : 1;
+        return this.randomizedDamage();
+    }
+
+    randomizedDamage(){
+        const multiplier = this.critical ? this.criticalMultiplier : 1;
         const d = this.baseDamage;
-        return game.rnd.integerInRange(Math.floor(d - d / 5), Math.floor(d + d / 5)) * multplier;
+        return game.rnd.integerInRange(Math.floor(d - d / 5), Math.floor(d + d / 5)) * multiplier;
     }
 
     update() {
         this.game.physics.arcade.collide(this, this.game.walls, () => this.kill());
     }
+
+
 }
